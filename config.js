dd005966a6454ac802bd5c3f559c2c4772af0a34
Hiteshyadav20 <162974120+Hiteshyadav20@users.noreply.github.com>
2025-12-31 13:09:51 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+export const MONGO_URL = process.env.MONGO_URL;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const RZP_KEY = process.env.RZP_KEY;
+export const RZP_SECRET = process.env.RZP_SECRET;
